f535b9996842a7d08e75012660979099f3c39d77
leondgarse <leondgarse@gmail.com>
2022-03-05 11:09:37 +0800

    update links

--- get_shadow_sockets.py
+++ get_shadow_sockets.py
@@ -4,6 +4,9 @@ import requests
 import re
 
 URL_DICT = {
+    "128": [100, "http://104.128.81.6:8080/clash/proxies?nc=CN"],
+    "158": [100, "http://158.101.93.192/clash/proxies?nc=CN"],
+    "149": [100, "http://149.248.8.112/clash/proxies?nc=CN"],
     # "135": [0, "http://8.135.91.61/clash/proxies?nc=CN"],
     # "136": [10, "http://8.136.5.236/clash/proxies?nc=CN"],
     # "233660": [10, "https://233660.xyz/clash/proxies?nc=CN"],
@@ -11,19 +14,20 @@ URL_DICT = {
     # "45": [100, "http://45.88.12.120/clash/proxies"],
     # "998988": [100, "https://998988.xyz/clash/proxies?nc=CN"],
     "ednovas": [100, "https://ednovas.design/clash/proxies?nc=CN"],
+    "ednovas.xyz": [0, "https://proxy.ednovas.xyz/clash/proxies?nc=CN"],
     # "freeu": [10, "https://freeu.xyz/clash/proxies?nc=CN"],
     "free.zdl": [10, "https://free.zdl.im/clash/proxies?nc=CN"],
     "free886": [0, "https://free886.herokuapp.com/clash/proxies?nc=CN"],
     # "getproxy": [100, "https://getproxy.olivers.works/clash/proxies?nc=CN"],
     "lonxin": [100, "https://fq.lonxin.net/clash/proxies?nc=CN"],
     # "luoml": [10, "https://emby.luoml.eu.org/clash/proxies?nc=CN"],
-    # "proxypool": [100, "https://proxypool.ednovas.xyz/clash/proxies?nc=CN"],
-    # "proxypool.fly": [0, "https://proxypool.fly.dev/clash/proxies?nc=CN"],
+    "proxypoolss.fly": [0, "https://proxypoolss.fly.dev/clash/proxies?nc=CN"],
     # "purel": [20, "https://proxy.purel.in/clash/proxies?nc=CN"],
-    # "sspool": [100, "https://sspool.herokuapp.com/clash/proxies?nc=IN,CN"],
+    "sspool": [100, "https://sspool.herokuapp.com/clash/proxies?nc=IN,CN"],
     # "sspool.nl": [0, "https://sspool.nl/clash/proxies?nc=IN,CN"],
     # "stgod": [100, "https://hello.stgod.com/clash/proxies?nc=IN,CN"],
     "xhrzg2017": [0, "https://hk.xhrzg2017.xyz/clash/proxies?nc=CN"],
+    "zjzj": [0, "https://dl.zjzj.xyz/clash/proxies?nc=CN"],
     # "zxcyec": [0, "https://smart.zxcyec.top/clash/proxies?nc=CN"],
 }
 
