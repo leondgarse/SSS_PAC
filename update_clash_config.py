5dd20a48f05e729472a309877e17bf9700fad6c4
leondgarse <leondgarse@gmail.com>
2023-06-05 18:46:57 +0800

    add update_clash_config

---
+++ update_clash_config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+
+PROXY_GROUP_TOKENS = {
+    '🇭🇰 香港节点': "HK", '🇯🇵 日本节点': "JP", '🇺🇲 美国节点': "US", '🇨🇳 台湾节点': "CN", '🇸🇬 狮城节点': "SG",
+    '🇰🇷 韩国节点': "KR", '🌍 其他地区': "others", '♻️ 专线节点': "others", '♻️ 手动切换': "all", "♻️ 自动选择": "all",
+}
+ALL_TOKENS = [ii for ii in PROXY_GROUP_TOKENS.values() if ii not in ["all", "others", "none"]]
+
+
+def gen_empty(source_yaml="~/.config/clash/config.yaml", save_yaml="empty.yaml"):
+    with open(os.path.expanduser(source_yaml), encoding='utf-8') as ff:
+        basic = yaml.safe_load(ff.read())
+    basic['proxies'] = []
+    for cur in basic['proxy-groups']:
+        if cur['name'] in PROXY_GROUP_TOKENS:
+            cur["proxies"] = []
+
+    with open(save_yaml, 'w', encoding='utf-8') as ff:
+        yaml.safe_dump(basic, ff, allow_unicode=True)
+    print(">>>> Output:", save_yaml)
+
+
+def update_proxies(update_yaml='clash.yaml', save_yaml="config.yaml", basic_yaml="empty.yaml"):
+    with open(os.path.expanduser(basic_yaml), encoding='utf-8') as ff:
+        basic = yaml.safe_load(ff.read())
+
+    with open(update_yaml, encoding='utf-8') as ff:
+        updates = yaml.safe_load(ff.read())
+
+    proxies = [ii for ii in updates['proxies'] if isinstance(ii['port'], int)]
+    proxy_names = [ii['name'] for ii in proxies]
+    basic["proxies"] = proxies
+
+    auto_select = {"name": "♻️ 自动选择", "type": "url-test", "url": "http://www.google.com/", "interval": 300, "proxies": []}
+    basic['proxy-groups'].insert(0, auto_select)
+
+    for cur in basic['proxy-groups']:
+        if cur['name'] in PROXY_GROUP_TOKENS:
+            cur_token = PROXY_GROUP_TOKENS[cur['name']]
+            if cur_token == "others":
+                cur['proxies'] = [ii['name'] for ii in proxies if all([tt not in ii['name'] for tt in ALL_TOKENS])]
+            elif cur_token == "none":
+                cur['proxies'] = []
+            elif cur_token == "all":
+                cur['proxies'] = proxy_names.copy()
+            else:
+                cur['proxies'] = [ii['name'] for ii in proxies if cur_token in ii['name']]
+        elif "♻️ 自动选择" not in cur["proxies"]:
+            if cur["proxies"][0] == 'DIRECT':
+                cur["proxies"].append("♻️ 自动选择")
+            else:
+                cur["proxies"].insert(0, "♻️ 自动选择")
+
+    with open(save_yaml, 'w', encoding='utf-8') as ff:
+        yaml.safe_dump(basic, ff, allow_unicode=True)
+    print(">>>> Output:", save_yaml)
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-u", "--update_yaml", type=str, help="source yaml path for update", default="clash.yaml")
+    parser.add_argument("-s", "--save_yaml", type=str, help="target yaml path for saving.", default="config.yaml")
+    parser.add_argument("-e", "--empty_yaml", type=str, help="empty yaml path containing rules.", default=os.path.join(FILE_PATH, "empty.yaml"))
+
+    args = parser.parse_args(sys.argv[1:])
+    update_proxies(update_yaml=args.update_yaml, save_yaml=args.save_yaml, basic_yaml=args.empty_yaml)
